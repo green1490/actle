719cc8ff59d00200dc7b53ad19dba108c0ffad99
green1490 <zambokrisztian1490@gmail.com>
2022-04-15 10:36:01 +0200

    implement tab widget

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,24 @@
-//refactor
-//üres textet ne lehessen megadni
 use eframe::{egui::{self},epi};
 
 struct TemplateApp  
 {
     title: String,
     button_label:String,
-    labels:Vec<String>
+    labels:Vec<String>,
+    learning_widget:bool,
+    question:String,
+    answer:String
 }
 
 impl Default  for TemplateApp  {
     fn default() -> Self {
         Self {
-            title: "Hello World!".to_owned(),
+            title: "Actle".to_owned(),
             labels:Vec::new(),
-            button_label:String::new()
+            button_label:String::new(),
+            learning_widget: true,
+            question:String::new(),
+            answer:String::new()
         }
     }
 }
@@ -23,24 +27,27 @@ impl epi::App  for TemplateApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &epi::Frame) 
     {
            
-        egui::SidePanel::left("side panel")
+        egui::SidePanel::left("side panel").max_width(300.0)
         .show(ctx,|ui|
         {
-            if ui.button("+").clicked()
+            ui.add_space(6.3);
+            
+            ui.horizontal(|ui|
             {
-                if !self.labels.contains(&self.button_label.trim().to_string())
+                if ui.button("+").clicked()
                 {
-                    let text = self.button_label.trim().to_string().to_owned();
-                    if !text.is_empty()
+                    if !self.labels.contains(&self.button_label.trim().to_string())
                     {
-                        self.labels.push(text);
+                        let text = self.button_label.trim().to_string().to_owned();
+                        if !text.is_empty()
+                        {
+                            self.labels.push(text);
+                        }
                     }
                 }
-            }
-
-            ui.add(egui::widgets::TextEdit::singleline(&mut self.button_label));
-            
-
+    
+                ui.add(egui::widgets::TextEdit::singleline(&mut self.button_label));    
+            });
             ui.vertical(|ui| 
             {   
                 ui.add(egui::widgets::Separator::default());
@@ -49,7 +56,7 @@ impl epi::App  for TemplateApp {
                 {
                     if ui.button(item).clicked()
                     {
-                        println!("{}",item);
+                        //implement
                     }
                 }
                 
@@ -58,7 +65,34 @@ impl epi::App  for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui|
         {
-            
+            ui.with_layout(egui::Layout::from_main_dir_and_cross_align(
+                egui::Direction::RightToLeft, egui::Align::Min
+            ), |ui|
+            {
+                if ui.button("Add new card").clicked()
+                {
+                    self.learning_widget = false;
+                }
+
+                if ui.button("Learn cards").clicked()
+                {
+                    self.learning_widget = true;
+                }
+            });
+            ui.separator();
+
+            if self.learning_widget
+                {
+                    ui.vertical_centered(|ui|
+                    {
+                        ui.text_edit_singleline(&mut self.question);
+                        ui.text_edit_multiline(&mut self.answer);
+                    });
+                }
+                else 
+                {
+                    
+                }
         });
 
     }
